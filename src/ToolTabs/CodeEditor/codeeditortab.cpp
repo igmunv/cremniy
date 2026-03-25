9b0c832f5b655f73dffd0a86f7f5f31c9242a325
Altro-O <asdordotacs@gmail.com>
2026-03-25 22:13:41 +0300

    fix: use portable checkbox state signal

--- src/ToolTabs/CodeEditor/codeeditortab.cpp
+++ src/ToolTabs/CodeEditor/codeeditortab.cpp
@@ -95,7 +95,7 @@ CodeEditorTab::CodeEditorTab(FileDataBuffer* buffer, QWidget* parent)
     connect(m_searchPrevButton, &QPushButton::clicked, this, [this]() { findNext(false); });
     connect(m_searchNextButton, &QPushButton::clicked, this, [this]() { findNext(true); });
     connect(m_searchCloseButton, &QPushButton::clicked, this, &CodeEditorTab::closeSearchBar);
-    connect(m_matchCaseCheckBox, &QCheckBox::checkStateChanged, this, [this](Qt::CheckState) {
+    connect(m_matchCaseCheckBox, &QCheckBox::stateChanged, this, [this](int) {
         updateSearchUi();
     });
 
